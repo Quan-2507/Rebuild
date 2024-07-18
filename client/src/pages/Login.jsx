0acc38bcbe89f848c418308fe532051236e3511f
Quan <21130495@st.hcmuaf.edu.vn>
2024-07-19 00:38:21 +0700

    new

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { createBrowserHistory } from 'history';
+import imagePath from '../utils/image/logo.png'
+import '../index.css'
 import { MDBContainer, MDBCol, MDBRow, MDBBtn, MDBIcon, MDBInput, MDBCheckbox } from 'mdb-react-ui-kit';
-
 
 const Login = () => {
     const [username, setUsername] = useState('');
@@ -9,13 +10,9 @@ const Login = () => {
     const [error, setError] = useState('');
     const [socket, setSocket] = useState(null);
     const [isLoginSuccess, setIsLoginSuccess] = useState(false);
-    const [connected, setConnected] = useState(false);
-    // const history = useHistory();
     const [userList, setUserList] = useState([]);
     const history = createBrowserHistory();
 
-
-    // Khi component được tạo, thiết lập kết nối WebSocket
     useEffect(() => {
         const newSocket = new WebSocket("ws://140.238.54.136:8080/chat/chat");
 
@@ -25,7 +22,6 @@ const Login = () => {
         });
 
         return () => {
-            // Đóng kết nối WebSocket khi component bị hủy
             newSocket.close();
         };
     }, []);
@@ -35,13 +31,11 @@ const Login = () => {
     }
 
     const loginAndGetUserList = () => {
-        // Kiểm tra nếu chưa nhập tên đăng nhập hoặc mật khẩu thì hiển thị thông báo lỗi
         if (!username || !password) {
             setError('Vui lòng nhập tên đăng nhập và mật khẩu');
             return;
         }
 
-        // Kiểm tra nếu chưa kết nối đến WebSocket Server thì thông báo lỗi
         if (!socket || socket.readyState !== WebSocket.OPEN) {
             setError('Không thể kết nối đến WebSocket Server');
             return;
@@ -52,8 +46,6 @@ const Login = () => {
             data: {
                 event: 'LOGIN',
                 data: {
-                    // user: sessionStorage.getItem('username'),
-                    // code: sessionStorage.getItem('password'),
                     user: username,
                     pass: password,
                 },
@@ -62,9 +54,6 @@ const Login = () => {
         socket.send(JSON.stringify(loginData));
         console.log("Đã gửi thông tin login cho server")
 
-
-
-        // Gửi yêu cầu lấy danh sách user tới WebSocket Server
         const userList = {
             action: 'onchat',
             data: {
@@ -75,11 +64,11 @@ const Login = () => {
         console.log("Đã gửi yêu cầu lấy danh sách cho server")
         socket.onmessage = (event) => {
             const response = JSON.parse(event.data);
-            if (response && response.status === "success" &&  response.event === 'LOGIN') {
+            if (response && response.status === "success" && response.event === 'LOGIN') {
                 sessionStorage.setItem('relogin_code', btoa(response.data.RE_LOGIN_CODE));
                 console.log("Đã lưu relogin_code vào session")
             }
-            if (response && response.status === "error" &&  response.event === 'LOGIN') {
+            if (response && response.status === "error" && response.event === 'LOGIN') {
                 setError(response.mes);
                 console.log(response.mes);
             }
@@ -88,20 +77,16 @@ const Login = () => {
                 setUserList(users);
                 setIsLoginSuccess(true);
                 console.log("đã lưu vào users")
-                history.push('/', {userList : users},{setUserList : setUserList})
+                history.push('/', { userList: users }, { setUserList: setUserList })
                 console.log("Đã chuyển sang trang '/'")
                 setError(response.mes)
                 window.location.href = '/'
             }
         }
-
-
     }
 
-    // Sau khi đăng nhập thành công, set socket và lưu trữ thông tin đăng nhập
     useEffect(() => {
         if (isLoginSuccess) {
-            // Lưu trữ thông tin đăng nhập vào localStorage
             sessionStorage.setItem('username', username);
         }
 
@@ -109,72 +94,50 @@ const Login = () => {
             socket.onmessage = (event) => {
                 const responseData = JSON.parse(event.data);
                 if (responseData && responseData.status === "success") {
-                    // Đăng nhập thành công
-                    // setIsLoginSuccess(true);
-                    // Lưu trữ thông tin đăng nhập, ví dụ: lưu trữ token
-                    sessionStorage.setItem('relogin_code', btoa(responseData.data.RE_LOGIN_CODE)); // ma hoa
+                    sessionStorage.setItem('relogin_code', btoa(responseData.data.RE_LOGIN_CODE));
                 }
                 if (responseData && responseData.event === "LOGIN" && responseData.status === "error") {
                     setError(responseData.mes);
                     console.log(responseData.mes)
                 }
-
             };
         }
     }, [socket, isLoginSuccess, username, password]);
 
-
-
     return (
-        <MDBContainer fluid className="p-3 my-5 h-custom">
-            <MDBRow>
-                <MDBCol col='10' md='6'>
-                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" class="img-fluid" alt="Sample image" />
-                </MDBCol>
-                <MDBCol col='4' md='6'>
-                    <div className="d-flex flex-row align-items-center justify-content-center">
-                        <p className="lead fw-normal mb-0 me-3">Đăng ký với </p>
-                        <MDBBtn floating size='md' tag='a' className='me-2'>
-                            <MDBIcon fab icon='facebook-f' />
-                        </MDBBtn>
-                        <MDBBtn floating size='md' tag='a' className='me-2'>
-                            <MDBIcon fab icon='twitter' />
-                        </MDBBtn>
-                        <MDBBtn floating size='md' tag='a' className='me-2'>
-                            <MDBIcon fab icon='linkedin-in' />
-                        </MDBBtn>
-                    </div>
-                    <div className="divider d-flex align-items-center my-4">
-                        <p className="text-center fw-bold mx-3 mb-0">Hoặc</p>
-                    </div>
-                    <MDBInput wrapperClass='mb-4' id='formControlLg' size="lg"
-                              label="Username"
-                              icon="user"
-                              value={username}
-                              onChange={(e) => setUsername(e.target.value)}
-                    />
-                    <MDBInput wrapperClass='mb-4' id='formControlLg' type='password' size="lg"
-                              label="Password"
-                              icon="lock"
-                              value={password}
-                              onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <div className="d-flex justify-content-between mb-4">
-                        <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
-                        <a href="!#">Quên mật khẩu?</a>
-                    </div>
-                    <div className='text-center text-md-start mt-4 pt-2'>
-                        <MDBBtn className="mb-0 px-5" size='lg' onClick={loginAndGetUserList}
-                        >Đăng nhập</MDBBtn>
-                        <p className="small fw-bold mt-2 pt-1 mb-2">Bạn chưa có tài khoản ? <a href="/register" className="link-danger">Đăng ký</a></p>
-                    </div>
-                    {error && <div className="alert alert-danger mt-3">{error}</div>}
-                </MDBCol>
-
-            </MDBRow>
-
-
-        </MDBContainer>
+        <div className="login-page  d-flex align-items-center justify-content-center"> {/* Đưa ảnh và form đăng nhập xuống giữa */}
+            <MDBContainer fluid className="p-0 m-0 h-100">
+                <MDBRow className="h-100">
+                    <MDBCol col='12' md='7' className="d-flex align-items-center justify-content-center">
+                        <img src={imagePath} className="img-fluid" alt="Phone image" />
+                    </MDBCol>
+                    <MDBCol col='12' md='5' className="d-flex align-items-center justify-content-center">
+                        <div className="login-form-wrapper">
+                            <MDBInput wrapperClass='mb-4' label='Username' id='formControlLg' value={username} onChange={(e) => setUsername(e.target.value)} size="lg" />
+                            <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' value={password} onChange={(e) => setPassword(e.target.value)} size="lg" />
+                            <div className="d-flex justify-content-between mx-4 mb-4">
+                                <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
+                                <a href="!#">Quên mật khẩu?</a>
+                            </div>
+                            <MDBBtn className="mb-4 w-100" size="lg" onClick={loginAndGetUserList}>Đăng nhập</MDBBtn>
+                            <div className="divider d-flex align-items-center my-4">
+                                <p className="text-center fw-bold mx-3 mb-0">HOẶC</p>
+                            </div>
+                            <MDBBtn className="mb-4 w-100" size="lg" style={{ backgroundColor: '#3b5998' }}>
+                                <MDBIcon fab icon="facebook-f" className="mx-2" />
+                                Tiếp tục với Facebook
+                            </MDBBtn>
+                            <MDBBtn className="mb-4 w-100" size="lg" style={{ backgroundColor: '#c51f3d' }}>
+                                <MDBIcon fab icon="google" className="mx-2" />
+                                Tiếp tục với gmail
+                            </MDBBtn>
+                            <p className="small fw-bold mt-2 pt-1 mb-2">Bạn chưa có tài khoản ? <a href="/register" className="link-danger">Đăng ký ngay</a></p>
+                            {error && <div className="alert alert-danger mt-3">{error}</div>}
+                        </div>
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
+        </div>
     );
 };
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,28 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import {
-    MDBBtn,
-    MDBContainer,
-    MDBRow,
-    MDBCol,
-    MDBCard,
-    MDBCardBody,
-    MDBCardImage,
-    MDBInput,
-    MDBIcon,
-    MDBCheckbox
-}
-    from 'mdb-react-ui-kit';
-const Register = () => {
-    const [roomName, setRoomName] = useState("");
-    const [socket, setSocket] = useState(null);
-    const [isLoginSuccess, setIsLoginSuccess] = useState(false);
+import { MDBBtn, MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBCardImage, MDBInput, MDBIcon } from 'mdb-react-ui-kit';
+import '../index.css';
+import imagePath from '../utils/image/logobgfff.png';
 
+const Register = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-
+    const [socket, setSocket] = useState(null);
     const [error, setError] = useState('');
-
     const navigate = useNavigate();
 
     // Khi component được tạo, thiết lập kết nối WebSocket
@@ -41,7 +27,7 @@ const Register = () => {
     }, []);
 
     const handleRegister = () => {
-        // Gửi yêu cầu đăng nhập đến server WebSocket
+        // Gửi yêu cầu đăng ký đến server WebSocket
         const requestData = {
             action: "onchat",
             data: {
@@ -55,20 +41,17 @@ const Register = () => {
         socket.send(JSON.stringify(requestData));
     };
 
-    // Sau khi đăng nhập thành công, set socket và lưu trữ thông tin đăng nhập
+    // Sau khi đăng ký thành công, set socket và chuyển hướng đến trang login
     useEffect(() => {
         if (socket) {
             socket.onmessage = (event) => {
                 const responseData = JSON.parse(event.data);
                 if (responseData && responseData.event === 'REGISTER' && responseData.status === "success") {
-                    // Đăng kí thành công
-                    setIsLoginSuccess(true);
-                    // Lưu trữ thông tin đăng nhập, ví dụ: lưu trữ token
-                    navigate('/login'); // Chuyển đến trang chủ
-                    alert("Đăng kí thành công")
-                    window.location.href = '/login'
-                }
-                else {
+                    // Đăng ký thành công
+                    navigate('/login'); // Chuyển đến trang đăng nhập
+                    alert("Đăng ký thành công");
+                    window.location.href = '/login'; // Chuyển hướng dự phòng
+                } else {
                     setError(responseData.mes);
                 }
             };
@@ -76,48 +59,45 @@ const Register = () => {
     }, [socket]);
 
     return (
-
-        <MDBContainer style={{ marginTop: '100px' }} fluid>
-
-            <MDBCard className='text-black m-5' style={{ borderRadius: '25px', marginTop: '50px' }}>
-                <MDBCardBody>
-                    <MDBRow>
-                        <MDBCol md='10' lg='6' className='order-2 order-lg-1 d-flex flex-column align-items-center'>
-
-                            <p classNAme="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Đăng ký tài khoản</p>
-
-                            <div className="d-flex flex-row align-items-center mb-4">
-                                <MDBIcon fas icon="envelope me-3" size='lg'/>
-                                <MDBInput
-                                    value={username}
-                                    onChange={(e) => setUsername(e.target.value)}
-                                    label='Username' id='form2' type='email'/>
-                            </div>
-
-                            <div className="d-flex flex-row align-items-center mb-4">
-                                <MDBIcon fas icon="lock me-3" size='lg'/>
-
-                                <MDBInput
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    label='Password' id='form3' type='password'/>
-                            </div>
-                            <MDBBtn className='mb-4' size='lg' onClick={handleRegister}>Đăng ký</MDBBtn>
-                            <p className="small fw-bold mt-2 pt-1 mb-2">Bạn chưa có tài khoản ? <a href="/login" className="link-danger">Đăng nhập</a></p>
-
-                            {error && <div className="alert alert-danger mt-3">{error}</div>}
-
-                        </MDBCol>
-
-                        <MDBCol md='10' lg='6' className='order-1 order-lg-2 d-flex align-items-center'>
-                            <MDBCardImage src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp' fluid />
-                        </MDBCol>
-
-                    </MDBRow>
-                </MDBCardBody>
-            </MDBCard>
-
-        </MDBContainer>
+        <div className="register-page">
+            <MDBContainer fluid>
+                <MDBCard className='text-black m-5' style={{ borderRadius: '25px', marginTop: '50px' }}>
+                    <MDBCardBody>
+                        <MDBRow>
+                            <MDBCol md='10' lg='6' className='order-2 order-lg-1 d-flex flex-column align-items-center'>
+                                <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Đăng ký tài khoản</p>
+                                <div className="d-flex flex-row align-items-center mb-4">
+                                    <MDBIcon fas icon="envelope me-3" size='lg' />
+                                    <MDBInput
+                                        value={username}
+                                        onChange={(e) => setUsername(e.target.value)}
+                                        label='Username'
+                                        id='form2'
+                                        type='email'
+                                    />
+                                </div>
+                                <div className="d-flex flex-row align-items-center mb-4">
+                                    <MDBIcon fas icon="lock me-3" size='lg' />
+                                    <MDBInput
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        label='Password'
+                                        id='form3'
+                                        type='password'
+                                    />
+                                </div>
+                                <MDBBtn className='mb-4' size='lg' onClick={handleRegister}>Đăng ký</MDBBtn>
+                                <p className="small fw-bold mt-2 pt-1 mb-2">Bạn đã có tài khoản ? <a href="/login" className="link-danger">Đăng nhập</a></p>
+                                {error && <div className="alert alert-danger mt-3">{error}</div>}
+                            </MDBCol>
+                            <MDBCol md='10' lg='5' className='order-1 order-lg-2 d-flex align-items-center'>
+                                <MDBCardImage src={imagePath} fluid />
+                            </MDBCol>
+                        </MDBRow>
+                    </MDBCardBody>
+                </MDBCard>
+            </MDBContainer>
+        </div>
     );
 };
 
